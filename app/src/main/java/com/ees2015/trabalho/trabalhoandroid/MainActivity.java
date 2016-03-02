fd7449a0d2564f19eed9ce51e53a8165e0178564
trabalhows <trabalhowsees2015@outlook.com>
2016-03-01 21:30:53 -0300

    Initial commit

---
+++ app/src/main/java/com/ees2015/trabalho/trabalhoandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ees2015.trabalho.trabalhoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
